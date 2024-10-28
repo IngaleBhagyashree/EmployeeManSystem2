c213575b2c5e3c8abeb4ad393a81e433c78d3682
Bhagyashree Ingale <ingalebhagyashree7@gmail.com>
2024-10-28 14:23:09 +0530

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'frontend_for_spring_boot';
+  title = 'Full Stack Project';
 }
